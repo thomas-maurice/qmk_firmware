e49b3546621eca589054d6d3ad5a1cd3ffe7d713
Thomas Maurice <thomas@maurice.fr>
2019-08-17 18:36:34 +0100

    Cleanup

--- keyboards/xd60/keymaps/thomas/config.h
+++ keyboards/xd60/keymaps/thomas/config.h
@@ -3,7 +3,12 @@
 
 #include "config_common.h"
 
-#define UNICODE_SELECTED_MODES UC_LNX, UC_WINC, UC_OSX
+#define UNICODE_SELECTED_MODES UC_LNX, UC_OSX, UC_WINC
+#define BACKLIGHT_BREATHING
+#define BREATHING_PERIOD 6
+#define TAPPING_TERM 200
+
+// Should probably be left on unless that becomes annoying
 //#define UNICODE_CYCLE_PERSIST false
 
 #endif
