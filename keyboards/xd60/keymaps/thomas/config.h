895df18188e70f3ec0a41b55d9a58a3ad605e914
Thomas Maurice <thomas@maurice.fr>
2019-08-17 11:08:34 +0100

    Adds emoji input and ascii faces

---
+++ keyboards/xd60/keymaps/thomas/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H_KEYMAP
+#define CONFIG_H_KEYMAP
+
+#include "config_common.h"
+
+#define UNICODE_SELECTED_MODES UC_LNX, UC_WINC, UC_OSX
+//#define UNICODE_CYCLE_PERSIST false
+
+#endif
--- keyboards/xd60/keymaps/thomas/keymap.c
+++ keyboards/xd60/keymaps/thomas/keymap.c
@@ -4,6 +4,44 @@
 #define _BASE 0
 #define _FN   1
 
+enum custom_keycodes {
+  SHRUG = SAFE_RANGE,
+  LENNY,
+  DISAP
+};
+
+enum unicode_names {
+    POOP
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [POOP]  = 0x1F4A9
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case SHRUG:
+            if (record->event.pressed) {
+                send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
+            }
+            break;
+        case LENNY:
+            if (record->event.pressed) {
+                send_unicode_hex_string("0028 0020 0361 00B0 0020 035C 0296 0020 0361 00B0 0029");
+            }
+            break;
+        case DISAP:
+            if (record->event.pressed) {
+                send_unicode_hex_string("0CA0 005F 0CA0");
+            }
+    }
+
+  return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all(
       KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_DEL,  KC_BSPC,   \
@@ -13,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL,  KC_LGUI, KC_LALT,                          KC_SPC,                          KC_RALT, KC_RCTL,  KC_LEFT, KC_DOWN, KC_RIGHT),
   [_FN] = LAYOUT_all(
       KC_TILDE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,    KC_F12,  KC_F13,  KC_F14,  \
-      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,          KC_TRNS, \
-      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_PGUP,   KC_TRNS,          KC_ENT, \
-      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_PGUP, KC_INS, \
-      KC_LCTL,    KC_LGUI, KC_LALT,                           KC_SPC,                          KC_RGUI, F(0), KC_TRNS, KC_PGDOWN,  KC_TRNS),
+      LENNY,      UNICODE_MODE_FORWARD,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, X(POOP),  KC_TRNS,   KC_TRNS, KC_TRNS, \
+      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_PGUP,   KC_TRNS,          SHRUG,   \
+      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_PGUP, KC_INS,  \
+      KC_LCTL,    DISAP,   KC_LALT,                           KC_SPC,                          KC_RGUI, F(0), KC_TRNS, KC_PGDOWN,  KC_TRNS),
 };
